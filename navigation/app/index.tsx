2c641390642edcbbd818bf5bf449c83087be8347
Rivindu_Pabasara <101160321+Rivindu10@users.noreply.github.com>
2025-01-05 14:10:24 +0530

    create Home Contactus AboutUs pages

--- navigation/app/index.tsx
+++ navigation/app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Home from '../components/Home';
+import ContactUs from '../components/ContactUs';
+import AboutUs from '../components/AboutUs';
 
+const Stack = createNativeStackNavigator();
 export default function Index() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='home'
+          screenOptions={{
+            headerStyle: { backgroundColor: '#e2bee2' },
+          }}
+        >
+          <Stack.Screen name='home' component={Home} options={{ title: "Welcome" }} />
+          <Stack.Screen name='contact' component={ContactUs} options={{ title: "Contact Us" }} />
+          <Stack.Screen name='aboutus' component={AboutUs} options={{ title: "About Us" }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+
   );
 }
